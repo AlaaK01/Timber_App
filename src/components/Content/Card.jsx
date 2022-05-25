29bdbbccf82a2ab237b3229d2960e84213b78873
Ahmedaadam <96ahmada@gafe.molndal.se>
2022-05-25 10:35:27 +0200

    Added Card

---
+++ src/components/Content/Card.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const Card = () => {
+  console.log('hej');
+  return (
+    <div className='container'>
+    <div class="card">
+    <h1>JobbAnnons</h1>
+      <img src='https://www.idg.se/editorial/0/path/1.717671.1574768828!imageUploader/3272512706.jpg' alt=" " style="width:100%"></img>
+    <p class="price">Ansök idag</p>
+    <p>Sök Tjänsten..</p>
+    <p><button onClick="https://se.indeed.com">Ansök</button></p>
+  </div>
+  </div>
+  )
+};
+
+export default Card
